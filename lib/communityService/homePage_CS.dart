5a74236be6d6080b7860e9f46e2aac4203e1602a
pankaj0695 <pankajgupta0695@gmail.com>
2024-07-21 21:44:21 +0530

    added CS and CR pages

--- lib/communityService/homePage_CS.dart
+++ lib/communityService/homePage_CS.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:uhv_app/communityService/mobile/com_chatscreen_mob.dart';
 
-class homePage_CS extends StatelessWidget {
+class homePage_CS extends StatefulWidget {
   const homePage_CS({super.key});
 
   @override
+  State<homePage_CS> createState() => _homePage_CSState();
+}
+
+class _homePage_CSState extends State<homePage_CS> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Community Service"),
-      ),
-      body: Center(child: Text("Community Service")),
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+            key: scaffoldKey,
+            backgroundColor: const Color(0xFF001120),
+            appBar: AppBar(title: const Text("Community Service")),
+            body: const ComChatScreenMob());
+      },
     );
   }
 }
--- lib/conflictResolution/homepage_CR.dart
+++ lib/conflictResolution/homepage_CR.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:uhv_app/conflictResolution/mobile/conf_chatscreen_mob.dart';
 
-class homePage_CR extends StatelessWidget {
+class homePage_CR extends StatefulWidget {
   const homePage_CR({super.key});
 
   @override
+  State<homePage_CR> createState() => _homePage_CRState();
+}
+
+class _homePage_CRState extends State<homePage_CR> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Conflict Resolution"),
-      ),
-      body: Center(child: Text("Conflict Resolution")),
+    final scaffoldKey = GlobalKey<ScaffoldState>();
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+            key: scaffoldKey,
+            backgroundColor: const Color(0xFF001120),
+            appBar: AppBar(
+              title: const Text("Conflict Resolution"),
+            ),
+            body: const ConfChatScreenMob());
+      },
     );
   }
 }
